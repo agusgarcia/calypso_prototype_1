8dd26a01bb5de9ce92eede22877e502f8d09ba4b
Agustina <geminis_agus@hotmail.com>
2017-03-03 07:14:39 +0100

    add UITween

---
+++ Assets/LevelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LevelController : MonoBehaviour, IPointerClickHandler {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public void OnPointerClick (PointerEventData eventData) {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
